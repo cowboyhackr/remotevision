7ed22069f111481866b2d6ff922f0df9d995502c
root <john.m.mckenna@live.com>
2014-09-23 00:20:56 -0500

    first commit

---
+++ stream-server.js
@@ -0,0 +1,121 @@
+
+if( process.argv.length < 3 ) {
+	console.log(
+		'Usage: \n' +
+		'node stream-server.js <secret> [<stream-port> <websocket-port>]'
+	);
+	process.exit();
+}
+
+var STREAM_SECRET = process.argv[2],
+	STREAM_PORT = process.argv[3] || 8082,
+	WEBSOCKET_PORT = process.argv[4] || 8084,
+	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+
+var width = 320,
+	height = 240;
+
+	//server some html
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+app.use(function(request, respond, next){
+  //whatever you put here will be executed
+  //on each request
+  	var params = request.url.substr(1).split('/');
+	width = (params[1] || 320)|0;
+	height = (params[2] || 240)|0;
+
+	if( params[0] == STREAM_SECRET ) {
+		console.log(
+			'Stream Connected: ' + request.socket.remoteAddress + 
+			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
+		);
+		request.on('data', function(data){
+			console.log("data received.");
+			console.log(data.length);
+			socketServer.broadcast(data, {binary:true});
+		});
+	}
+	else {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + 
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		//response.send("hello?");
+	}
+
+  //next();  // BE SURE TO CALL next() !!
+});
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+//end serve some html
+
+// Websocket Server
+var socketServer = new (require('ws').Server)({server : server});
+socketServer.on('connection', function(socket) {
+	console.log("websocket server created.")
+	// Send magic bytes and video size to the newly connected socket
+	// struct { char magic[4]; unsigned short width, height;}
+	var streamHeader = new Buffer(8);
+	streamHeader.write(STREAM_MAGIC_BYTES);
+	streamHeader.writeUInt16BE(width, 4);
+	streamHeader.writeUInt16BE(height, 6);
+	socket.send(streamHeader, {binary:true});
+
+	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
+	
+	socket.on('close', function(code, message){
+		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
+	});
+});
+
+socketServer.broadcast = function(data, opts) {
+	console.log('receive data');
+	console.log(data.length);
+	for( var i in this.clients ) {
+		if (this.clients[i].readyState == 1) {
+			this.clients[i].send(data, opts);
+		}
+		else {
+			console.log( 'Error: Client ('+i+') not connected.' );
+		}
+	}
+};
+
+
+// HTTP Server to accept incomming MPEG Stream
+/*var streamServer = require('http').createServer( function(request, response) {
+	var params = request.url.substr(1).split('/');
+	width = (params[1] || 320)|0;
+	height = (params[2] || 240)|0;
+
+	if( params[0] == STREAM_SECRET ) {
+		console.log(
+			'Stream Connected: ' + request.socket.remoteAddress + 
+			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
+		);
+		request.on('data', function(data){
+			console.log("data received.");
+			console.log(data.length);
+			socketServer.broadcast(data, {binary:true});
+		});
+	}
+	else {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + 
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		response.end();
+	}
+}).listen();*/
+
+
